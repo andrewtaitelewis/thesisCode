d287911c485058a5540b3afeefd807c0430f91c8
Andrew <andrewtaitelewis@gmail.com>
2025-03-20 11:25:58 -0400

    made new way with determinants instead

--- lineCrosser.py
+++ lineCrosser.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import scipy
-
+import time 
 #Trying to do a 
 
 class line:
@@ -22,6 +22,9 @@ class line:
         '''
         return lambda s: np.asarray([(self.x2-self.x1)*s +self.x1, (self.y2-self.y1)*s + self.y1])
 
+    def returnPoints(self):
+        '''Returns the points which define the line'''
+        return self.x1,self.y1,self.x2,self.y2
 def distance(line1,line2,s,t):
     '''
     returns the distance between two lines
@@ -29,15 +32,37 @@ def distance(line1,line2,s,t):
     return np.sum((line1(s) - line2(t))**2)
 def intersectionChecker(line1,line2):
     '''Given two lines.... do they intersect'''
+    toMinimize = lambda s: distance(line1,line2,s[0],s[1])  #Define our minimization function
+    minimization = (scipy.optimize.minimize(toMinimize,[0.5,0.5],bounds = [(0,1),(0,1)]))   #minimize this
+    #if the molecule crosses the barrier return true, else return false
+    if minimization['fun'] < 1e-15:
+        return True 
+    else:  
+        return False
 
 
 
 
-myline = line(-1,1,1,-1,0); myline2 = line(-1,-1,1,1)
+
+def intersectionPoints(line1,line2):
+    x1,y1,x2,y2 = line1.returnPoints(); x3,y3,x4,y4 = line2.returnPoints()
+    print('hello')
+    #So there is a chance that it'll be parallel
+    try:
+        px = ((x1*y2 - y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4))
+        py = ((x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4))
+    except:
+        print('Lines are Parallel')
+        return (0,0)
+    return px,py
+
+
+
+myline = line(-1,1,1,-1,0); myline2 = line(-1,-1,-4,-4)
 func = myline.vectorize(); func2= myline2.vectorize()
 print(func(0.5))
 
-print(distance(func,func2,0.5,0.5))
+print(intersectionPoints(myline,myline2))
 
 
 #Now we determine if the distance functional can be properly minimized
@@ -46,7 +71,9 @@ toMinimize = lambda s: distance(func,func2,s[0],s[1])
 
 x0 = [0,0]
 minimization = (scipy.optimize.minimize(toMinimize,x0))
-print(minimization['fun'] < 1e-15)
+time1 = time.time()
+print(intersectionChecker(func,func2))
+print(time.time() - time1)
 #Test Case
 #Line1 (-1,1) -> (1,-1)
 #Line2 (-1,-1) -> (1,1)
